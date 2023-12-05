ec08d39152159e475e6e2f74d02b2ce1604f6a76
gabriel-poatek <gabriel.ximenes@poatek.com>
2023-12-05 11:36:12 -0300

    avancando no codigo

--- app/src/main/java/br/com/fiap/aluvery/MainActivity.kt
+++ app/src/main/java/br/com/fiap/aluvery/MainActivity.kt
@@ -5,31 +5,42 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.horizontalScroll
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.material3.MaterialTheme
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment.Companion.CenterHorizontally
+import androidx.compose.ui.Alignment.Companion.BottomCenter
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Brush
-import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import br.com.fiap.aluvery.extensions.toBrazilianCurrency
+import br.com.fiap.aluvery.model.Product
 import br.com.fiap.aluvery.ui.theme.AluveryTheme
 import br.com.fiap.aluvery.ui.theme.Pink80
-import br.com.fiap.aluvery.ui.theme.Purple40
 import br.com.fiap.aluvery.ui.theme.Purple80
+import java.math.BigDecimal
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -38,7 +49,7 @@ class MainActivity : ComponentActivity() {
             AluveryTheme {
                 // A surface container using the 'background' color from the theme
                 Surface {
-                    ProductItem()
+                    ProductsSection()
                 }
             }
         }
@@ -46,40 +57,125 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun ProductItem() {
-    Column(
-        Modifier
-            .height(250.dp)
-            .width(150.dp)
+fun ProductItem(product: Product) {
+    Surface(
+        shape = RoundedCornerShape(16.dp),
+        shadowElevation = 4.dp,
     ) {
-        Box(
-            modifier = Modifier
-                .height(50.dp)
-                .background(
-                    brush = Brush.horizontalGradient(
-                        colors = listOf(
-                            Purple80,
-                            Pink80
+        Column(
+            Modifier
+                .heightIn(250.dp, 300.dp)
+                .width(200.dp)
+        ) {
+            val imageSize = 100.dp
+            Box(
+                modifier = Modifier
+                    .height(imageSize)
+                    .background(
+                        brush = Brush.horizontalGradient(
+                            colors = listOf(
+                                Purple80,
+                                Pink80
+                            )
                         )
                     )
-                )
-                .fillMaxWidth()
+                    .fillMaxWidth()
 
-        )
-        Image(
-            painter = painterResource(id = R.drawable.ic_launcher_background),
-            contentDescription = "acessibilidade",
-            Modifier.size(100.dp)
-                .offset(y = (-50).dp)
-                .clip(CircleShape)
-                .align(alignment = CenterHorizontally)
-        )
-        Text(text = "Nome do produto")
+            ) {
+                Image(
+                    painter = painterResource(id = product.image),
+                    contentDescription = "acessibilidade",
+                    Modifier
+                        .size(imageSize)
+                        .offset(y = imageSize / 2)
+                        .clip(CircleShape)
+                        .align(BottomCenter),
+                    contentScale = ContentScale.Crop
+                )
+            }
+            Column(
+                Modifier.padding(16.dp)
+            ) {
+                Spacer(modifier = Modifier.height(imageSize / 2))
+                Text(
+                    text = product.name,
+                    fontSize = 18.sp,
+                    fontWeight = FontWeight(700),
+                    maxLines = 2,
+                    overflow = TextOverflow.Ellipsis
+                )
+                Text(
+                    text = product.price.toBrazilianCurrency(),
+                    Modifier.padding(top = 8.dp),
+                    fontSize = 14.sp,
+                    fontWeight = FontWeight(400)
+                )
+            }
+
+        }
+
     }
 }
 
-@Preview
+@Preview(showBackground = true)
 @Composable
 fun ProductItemPreview() {
-    ProductItem()
+    ProductItem(
+        Product(
+            name = "Produto 1",
+            price = BigDecimal(100),
+            image = R.drawable.ic_launcher_background
+        )
+    )
+}
+
+@Composable
+fun ProductsSection() {
+    Column {
+        Text(
+            text = "Promoções",
+            Modifier.padding(start = 16.dp, top = 16.dp, end = 16.dp),
+            fontSize = 20.sp,
+            fontWeight = FontWeight(400)
+        )
+        Row(
+            Modifier
+                .padding(
+                    top = 8.dp,
+                    bottom = 16.dp
+                )
+                .fillMaxWidth()
+                .horizontalScroll(rememberScrollState())
+                .padding(horizontal = 16.dp),
+            horizontalArrangement = Arrangement.spacedBy(16.dp)
+        ) {
+            ProductItem(
+                product = Product(
+                    name = "Hambúrguer",
+                    price = BigDecimal("25.00"),
+                    image = R.drawable.burger
+                )
+            )
+            ProductItem(
+                product = Product(
+                    name = "Fritas",
+                    price = BigDecimal("10.00"),
+                    image = R.drawable.fries
+                )
+            )
+            ProductItem(
+                product = Product(
+                    name = "Pizza",
+                    price = BigDecimal("35.00"),
+                    image = R.drawable.pizza
+                )
+            )
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun ProductsSectionPreview() {
+    ProductsSection()
 }
---
+++ app/src/main/java/br/com/fiap/aluvery/model/Product.kt
@@ -0,0 +1,13 @@
+package br.com.fiap.aluvery.model
+
+import androidx.annotation.DrawableRes
+import java.math.BigDecimal
+
+data class Product(
+    val name: String,
+    val price: BigDecimal,
+    @DrawableRes
+    val image: Int
+)
+
+
